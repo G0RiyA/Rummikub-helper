3869a1132d83a5024ffd7d0928c6bd62bd40c474
G0RiyA <g0y4ng2@kakao.com>
2020-01-16 11:47:53 +0900

    init

---
+++ finder.py
@@ -0,0 +1,17 @@
+import itertools
+
+class card:
+    def __init__(self):
+        pass
+
+class combo:
+    def __init(self):
+        pass
+
+def findAllLinear():
+    pass
+
+def findAllColor():
+    pass
+
+
